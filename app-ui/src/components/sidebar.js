3d3813ce7458a9874051d7535da44b6124f75ac9
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2023-05-21 23:30:33 +0530

    Create sidebar.js

---
+++ app-ui/src/components/sidebar.js
@@ -0,0 +1,65 @@
+import { Dashboard } from "./dashboard";
+import { Settings } from "./settings";
+import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
+import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
+import { Sidenav, Nav, Sidebar, Container, Content } from 'rsuite';
+import React, { useEffect } from "react";
+
+const DashboardSidebar = ({ appearance, expanded, onExpand, ...navProps }) => {
+  return (
+    <Sidenav
+      appearance={appearance}
+      expanded={expanded}
+    >
+      <Sidenav.Body>
+        <Nav {...navProps}>
+          <Nav.Item eventKey="1" active icon={<DashboardIcon />}>
+            Dashboard
+          </Nav.Item>
+          <Nav.Item eventKey="2" icon={<GearCircleIcon />}>
+            Settings
+          </Nav.Item>
+        </Nav>
+      </Sidenav.Body>
+      <Sidenav.Toggle onToggle={onExpand} />
+    </Sidenav>
+  );
+};
+
+const DashboardPageHandler = (props) => {
+  const [activeKey, setActiveKey] = React.useState('1');
+  const [expanded, setExpand] = React.useState(true);
+  const [pageContent, setPageContent] = React.useState('');
+
+  useEffect(() => {
+    if(activeKey === '1') {
+      setPageContent(<Dashboard userInfo={props.userInfo}/>)
+    } else if(activeKey === '2') {
+      setPageContent(<Settings/>)
+    }
+  }, [activeKey])
+
+  return (
+    <>
+      <Container>
+        <Sidebar width={expanded ? 260 : 56}>
+          <DashboardSidebar
+            activeKey={activeKey}
+            onSelect={setActiveKey}
+            expanded={expanded}
+            onExpand={setExpand}
+            appearance="subtle"
+          />
+        </Sidebar>
+        
+        <Container className="page-container">
+          <Content>
+            {pageContent}
+          </Content>
+        </Container>
+      </Container>
+    </>
+  );
+};
+
+export {DashboardSidebar, DashboardPageHandler};
